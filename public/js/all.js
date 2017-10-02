4fc527481da23735f977c7f6ac768e47afe88a9a
Niloufar <nw.tahmasebi@gmail.com>
2017-10-02 16:16:50 +0330

    some primary changes

--- public/js/all.js
+++ public/js/all.js
@@ -1645,6 +1645,7 @@ app.controller('CustomerAddCtrl', function ($scope, htp, $controller, notify, $r
             notify('info', 'کد مشتری ثبت شده :' + response.id);
     };
 
+
     var GROUP = function () {
         this.text = undefined;
         this.parent = undefined;
@@ -1720,95 +1721,10 @@ app.controller('CustomerAddCtrl', function ($scope, htp, $controller, notify, $r
         });
     }
 });
-app.controller('CustomerListCtrl', function ($scope, htp, $mdDialog, notify) {
+app.controller('CustomerListCtrl', function ($scope) {
     var _this = $scope;
     _this.tbl = {};
-    _this.orderDialog = function (row, ev) {
-        var dialog = $mdDialog.show({
-                controller: function ($scope, $controller, dt, $mdDialog) {
-                    $scope.dt = dt;
-                    $scope.edit_mode = true;
-                    $scope.data = {};
-                    $scope.hide = function () {
-                        $mdDialog.hide();
-                    };
-                    $scope.cancel = function () {
-                        $mdDialog.cancel();
-                    };
-                    $scope.tbl = {};
-                    $scope.tbl.postData = function () {
-                        return {
-                            uid: dt.id
-                        }
-                    };
 
-                    var FACTS = function () {
-                        this.paid = this.unpaid = this.loyalty = undefined;
-                        var that = this;
-                        htp(home('console/customer/get-facts-data'), {uid: dt.id}).then(function (response) {
-                            if (response && response.result == true) {
-                                that.data = response.facts;
-                            }
-                        });
-                    };
-                    $scope.facts = new FACTS();
-                },
-                templateUrl: home('console/customer/get-order-data'),
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose: true,
-                locals: {
-                    dt: row
-                },
-                fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-            })
-            .then(function (answer) {
-
-            });
-    };
-
-
-    _this.ibanDialog = function (row, ev) {
-        var dialog = $mdDialog.show({
-                controller: function ($scope, $controller, dt, $mdDialog) {
-                    $scope.dt = dt;
-                    $scope.edit_mode = true;
-                    $scope.hide = function () {
-                        $mdDialog.hide();
-                    };
-                    $scope.cancel = function () {
-                        $mdDialog.cancel();
-                    };
-                    $scope.editBtnDialog = function () {
-                        $scope.preloader = true;
-                        htp(home('console/edit-dialog'), {
-                            data: $scope.data
-                        }).then(function (response) {
-                            $scope.preloader = false;
-                            $mdDialog.hide();
-                        });
-                    };
-                    // $controller(ctrl, {$scope: $scope});
-                    $scope.init = function ($id) {
-                        htp(home('console/customer/get-iden-info'), {id: $id}).then(function (response) {
-                            console.log('response', response);
-                            $scope.ibans = response;
-                        });
-                    };
-                },
-                templateUrl: home('console/customer/iban-info/' + row.id),
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose: true,
-                locals: {
-                    dt: row
-                },
-                fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-            })
-            .then(function (answer) {
-
-            });
-    }
 });
 app.controller('CustomerGroupCtrl', function ($scope, htp , notify) {
     var _this = $scope;
@@ -1873,10 +1789,9 @@ app.controller('LogCtrl', function ($scope, htp) {
 });
 app.controller('OrderListCtrl', function ($scope, htp, $mdDialog, NgTableParams) {
     var _this = $scope;
-
     _this.tbl = {};
 
-
+    
     _this.setVisitor = function (row) {
         row.visitor_loading = true;
         htp(home('console/order/set-visitor'), {id: row.id}).then(function (response) {

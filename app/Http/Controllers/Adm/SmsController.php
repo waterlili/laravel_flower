926c95d29ee79e30917921fcace05531da5d7e3a
NiloufarTahmasebi <nw.tahmasebi@gmail.com>
2017-11-08 13:32:44 +0330

    some changes about payment

--- app/Http/Controllers/Adm/SmsController.php
+++ app/Http/Controllers/Adm/SmsController.php
@@ -24,13 +24,20 @@ class SmsController extends Controller
                     http://185.173.106.234/payment/$query/zarinpal";
         $api = new KavenegarApi("707041326F734C485A346D5165672F616C656C4471413D3D");
         $res = $api->Send($sender, $receptor, $message);
-        $status = $res[0]->status;
+        $type = gettype($res);
+        if ($type == 'array')
+            $status = $res[0]->status;
+        else
+            $code = $res->getCode();
+
+
+
         $message1 = "خطا در ارسال";
         $message2 = "در حال ارسال";
 
-        if ($status != 1) {
+        if (!empty($code) && $code != 200) {
             return response()->json(['message', $message1], 422);
-        } else {
+        } else if (!empty($status) && $status == 1) {
             return response()->json(['message', $message2], 200);
 
         }

7cda8c43f18de9d2f21a9a6abf089c7aa4553ea4
NiloufarTahmasebi <nw.tahmasebi@gmail.com>
2017-12-02 10:41:08 +0330

    delete automaticly excel file

--- app/Http/Controllers/Adm/AdminController.php
+++ app/Http/Controllers/Adm/AdminController.php
@@ -51,7 +51,7 @@ class AdminController extends Controller {
      */
     public function getExcelFile(Request $request) {
         if ($request->has('url')) {
-            $job = (new RemoveExcelForm($request->input('url')))->delay(Carbon::now()->addSecond(60));
+            $job = (new RemoveExcelForm($request->input('url')))->delay(Carbon::now()->addSeconds(60));
             dispatch($job);
             return response()->download(storage_path('app/excel/export') . '/' . $request->input('url'));
         }

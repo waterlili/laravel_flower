150f7e821b8b9045710eb6725738b373bbce727d
NiloufarTahmasebi <nw.tahmasebi@gmail.com>
2017-12-17 21:39:35 +0330

    report for confirm sending

--- resources/assets/js/Ctrl/Admin/CustomerCtrl.js
+++ resources/assets/js/Ctrl/Admin/CustomerCtrl.js
@@ -20,101 +20,12 @@ app.controller('CustomerAddCtrl', function ($scope, htp, $controller, notify, $r
         });
     }
 
-
-    $scope.$on('child1', function (event, data) {
-        // _this.lastAddress = [];
-        // console.log(data);
-
-        _this.arr = [];
-        _this.arr = data;
-        _this.information.push(_this.arr);
-        _this.lastAddress = _this.information[_this.information.length - 1];
-
-
-    });
-    $scope.$on('child2', function (event, data) {
-        // _this.lastAddress = [];
-        // console.log(data);
-
-        _this.arr1 = [];
-        _this.arr1 = data;
-        _this.information1.push(_this.arr1);
-        _this.lastAddress1 = _this.information1[_this.information1.length - 1];
-
-
-    });
-    $scope.$on('child3', function (event, data) {
-        // _this.lastAddress = [];
-        // console.log(data);
-
-        _this.arr2 = [];
-        _this.arr2 = data;
-        _this.information2.push(_this.arr2);
-        _this.lastAddress2 = _this.information2[_this.information2.length - 1];
-
-
-    });
-    // $scope.$on('child1', function (event, data) {
-    //     // var $export = [];
-    //     _this.information.push(data);
-    //     console.log(_this.information);
-    //
-    // });
-    // $scope.$on('child2', function (event, data) {
-    //     // var $export = [];
-    //     _this.information.push(data);
-    //     console.log(_this.information);
-    //
-    // });
-
     _this.afterSuccess = function (response) {
 
         if (response.id)
             notify('info', 'کد مشتری ثبت شده :' + response.id);
     };
 
-
-    // var GROUP = function () {
-    //     this.text = undefined;
-    //     this.parent = undefined;
-    //     this.all = [];
-    //     var that = this;
-    //     this.init = function () {
-    //         htp(home('console/customer/get-groups')).then(function (response) {
-    //             if (response && response.result == true) {
-    //                 that.all = response.data;
-    //             }
-    //         });
-    //     };
-    //     this.add = function () {
-    //         this.loading = true;
-    //         var that = this;
-    //         that.errorItem = undefined;
-    //         htp(home('console/customer/add-group'), {
-    //             title: this.text,
-    //             parent: (this.parent) ? this.parent.id : null
-    //         }).then(function (response) {
-    //             that.text = '';
-    //             that.init();
-    //         }).error(function (response, sts) {
-    //             if (sts == 422) {
-    //                 that.errorItem = response;
-    //             }
-    //         }).after(function (response) {
-    //             that.loading = false;
-    //         });
-    //     };
-    //     this.init();
-    // };
-    // _this.group = new GROUP();
-    //
-    // _this.export = function () {
-    //     var arr = _this.findDeep(_this.group.all);
-    //     _this.data.groups = _.map(arr, function (val) {
-    //         return val.id;
-    //     });
-    //     return _this.data;
-    // };
 
     _this.findDeep = function (items) {
         var $export = [];
